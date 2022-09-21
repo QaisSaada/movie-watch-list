1aaf8de2fc0d117be13b0cd6fa0d27124195b463
Qais Saada <qais.saada@karnovgroup.com>
2022-09-21 13:38:42 +0200

    adds model assosiations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
   validates :name, uniqueness: true
+
+  belongs_to :user
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, :through :bookmarks
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, uniqueness: true
   validates :overview, uniqueness: true
+
+  has_many :bookmarks, dependent: :destroy
+
 end
