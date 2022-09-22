d082e4229d7661558a716e33b8d78e8ebe087b92
Qais Saada <qais.saada@karnovgroup.com>
2022-09-22 11:14:35 +0200

    adds routes controller and view to bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :lists  
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :movies
 
 
 end
