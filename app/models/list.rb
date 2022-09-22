6e9d77c7edead7e96e2371407e44399ec6a351f8
Qais Saada <qais.saada@karnovgroup.com>
2022-09-22 10:25:51 +0200

    fixes schcima error

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,5 @@ class List < ApplicationRecord
 
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
-  has_many :movies, :through :bookmarks
+  has_many :movies, through: :bookmarks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_100950) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_082043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_100950) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_100950) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
 end
